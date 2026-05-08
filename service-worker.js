ba5a9e60a97234e6c3d8a600d2972cd2ce8e718c
JustGoodinc <108307855+JustGoodinc@users.noreply.github.com>
2026-05-08 01:54:37 -0700

    Refactor service worker event listeners for clarity

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,19 @@
-self.addEventListener("install", (event) => self.skipWaiting());
-self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then((names) => Promise.all(names.map((name) => caches.delete(name)))).then(() => self.clients.claim()));
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
 });
-self.addEventListener("fetch", (event) => event.respondWith(fetch(event.request)));
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request));
+});
